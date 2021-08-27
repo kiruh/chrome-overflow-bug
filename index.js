50e8cb3aae08a500d44ca09d35d5d7fc0b6df805
Kiril Nedelev <nedelev.kiril@gmail.com>
2021-08-28 01:52:25 +0300

    hide menu bar

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ const { app, BrowserWindow } = require("electron");
 function createWindow() {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
+    width: 600,
     height: 600,
+    autoHideMenuBar: true,
   });
 
   // and load the index.html of the app.
